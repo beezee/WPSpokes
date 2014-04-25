a1fd60bdb4c4b8030d104b7d8f01f5ff40b050a8
Brian Zeligson <bzeligson@localytics.com>
2014-04-25 09:45:26 -0400

    whitespace

--- WPSpokes/Framework/Models/User.php
+++ WPSpokes/Framework/Models/User.php
@@ -5,51 +5,51 @@ namespace WPSpokes\Framework\Models;
 class User extends \WPSpokes\Framework\Model
 {
 
-    protected $primaryKey = 'ID';
-    protected $table = 'wp_users';
+  protected $primaryKey = 'ID';
+  protected $table = 'wp_users';
 
-	public function usermeta()
-	{
-		return $this->hasMany('\WPSpokes\Framework\Models\UserMeta', 'user_id');
-	}
+  public function usermeta()
+  {
+    return $this->hasMany('\WPSpokes\Framework\Models\UserMeta', 'user_id');
+  }
 
-    public function add_filters_to($filter_chain)
-    {
-        $filter_chain->filter(array('user_login', 'display_name', 'user_nicename'),
-            new \WPSpokes\Framework\Filters\StripTags());
-    }
+  public function add_filters_to($filter_chain)
+  {
+      $filter_chain->filter(array('user_login', 'display_name', 'user_nicename'),
+          new \WPSpokes\Framework\Filters\StripTags());
+  }
 
-    public function add_rules_to(\Valitron\Validator $validator)
-    {
-        $validator->rule('required',
-            array('user_login', 'user_pass', 'user_email', 
-                'user_nicename', 'user_registered', 'display_name'));
-        $validator->rule('email', 'user_email');
-        $validator->rule('slug', 'user_nicename');
-        $validator->rule('dateFormat', 'user_registered', 'Y-m-d H:i:s');
-    }
+  public function add_rules_to(\Valitron\Validator $validator)
+  {
+      $validator->rule('required',
+          array('user_login', 'user_pass', 'user_email', 
+              'user_nicename', 'user_registered', 'display_name'));
+      $validator->rule('email', 'user_email');
+      $validator->rule('slug', 'user_nicename');
+      $validator->rule('dateFormat', 'user_registered', 'Y-m-d H:i:s');
+  }
 
-    public function scopeCurrent($query)
-    {
-        $query->where('ID', '=', get_current_user_id());
-    }
+  public function scopeCurrent($query)
+  {
+      $query->where('ID', '=', get_current_user_id());
+  }
 
-    public function set_password($password)
-    {
-        $this->user_pass = wp_hash_password($password);
-    }    
+  public function set_password($password)
+  {
+      $this->user_pass = wp_hash_password($password);
+  }    
 
-    public function password_is_valid($password)
-    {
-        return wp_check_password($password, $this->user_pass);
-    }
+  public function password_is_valid($password)
+  {
+      return wp_check_password($password, $this->user_pass);
+  }
 
-    public function validate()
-    {
-        if (!$this->user_registered)
-            $this->user_registered = date('Y-m-d H:i:s');
-        return parent::validate();
-    }
+  public function validate()
+  {
+      if (!$this->user_registered)
+          $this->user_registered = date('Y-m-d H:i:s');
+      return parent::validate();
+  }
 
 	public function roles()
 	{
